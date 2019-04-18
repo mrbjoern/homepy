727c519a652f0b39c7013502905c0c2c4261942f
Halvor Bjørn <halvbj@usit.uio.no>
2019-04-18 14:21:21 +0200

    Improves tests

--- api/__init__.py
+++ api/__init__.py
@@ -4,7 +4,7 @@ import logging
 from flask import Flask
 from flask_cors import CORS
 
-from api import api
+from api import hue_api
 
 
 log = logging.getLogger(__name__)
@@ -13,7 +13,7 @@ log = logging.getLogger(__name__)
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
-    app.register_blueprint(api.bp)
+    app.register_blueprint(hue_api.bp)
 
     CORS(app)
     return app
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,19 +1,3 @@
-import pytest
-
-from api import create_app
-
-
-@pytest.fixture
-def app():
-    app = create_app()
-    yield app
-
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
-
-
 def test_index(client):
     response = client.get('/hue/lights')
     assert response.data
